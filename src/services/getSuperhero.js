1f6b90ee8496b8cc21efcc637783201d04ed25b3
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2021-10-23 16:44:04 -0300

    API BASE_URL for deployment

--- src/services/getSuperhero.js
+++ src/services/getSuperhero.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 import { ACCESS_TOKEN } from './access'
 
 export default function getSuperhero ({ id = '1'} = {}) {
-    const BASE_URL = `/api/${ACCESS_TOKEN}/${id}`
+    //const BASE_URL = `/api/${ACCESS_TOKEN}/${id}` (localhost) with setupProxy
+    const BASE_URL = `https://www.superheroapi.com/api.php/${ACCESS_TOKEN}/${id}`
     const request = axios.get(BASE_URL)
     return request.then(response => response.data)
 }
--- src/services/searchByName.js
+++ src/services/searchByName.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 import { ACCESS_TOKEN } from './access'
 
 export default function searchByName ({ keyword = ''} = {}) {
-    const BASE_URL = `/api/${ACCESS_TOKEN}/search/${keyword}`
+    // const BASE_URL = `/api/${ACCESS_TOKEN}/search/${keyword}`    (localhost) with setupProxy
+    const BASE_URL = `https://www.superheroapi.com/api.php/${ACCESS_TOKEN}/search/${keyword}`
     const request = axios.get(BASE_URL)
     return request.then(response => response.data)
 }
